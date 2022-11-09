3e54a1815b31ee8b2630cd53ba902e03f84629af
MoreiraAlex <celerom100@gmail.com>
2022-11-09 14:19:31 -0300

    Adcionando e ajustando home

---
+++ src/layout/Home.js
@@ -0,0 +1,32 @@
+import styles from '../styles/layout/Main.module.css'
+
+import Image from 'next/image'
+
+import logo from '../../public/logo.svg'
+import card from '../assets/formasPagamento.svg'
+// import thumb from '../assets/Thumb1.jpg'
+
+export default function Home() {
+  return (
+    <main className={styles.main}>
+        <header>
+            <Image src={logo} width={100} height={100} alt='Logo do site' quality={100}/>
+        </header>
+        <section>
+            <h1>
+                se torne um herói<br/>
+                <span>de uma vez por todas</span>
+            </h1>
+            <p>
+                Lorem ipsum dolor sit amet, consectetur adipiscing elit.<br/>
+                Ut elit tellus, luctus nec ullamcorper mattis, pulvinar dapibus leo.
+            </p>
+        </section>
+        <section>
+            <video controls poster='/Thumb1.jpg'></video>
+            <button>quero me inscrever agora!</button>
+            <Image src={card} width={250} alt='Bandeiras de cartão de credito' />
+        </section>
+    </main>
+  )
+}
